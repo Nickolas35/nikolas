cfc84ad86c9b291e542168fb1bfcfdc810191659
Nickolas35 <86787140+Nickolas35@users.noreply.github.com>
2022-06-05 19:41:51 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
+let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
-mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 let my_Enemy = sprites.create(assets.image`myImage0`, SpriteKind.Enemy)
 my_Enemy.follow(mySprite)
