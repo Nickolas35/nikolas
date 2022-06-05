cc1be60b6d4ed42e7e459fa88c16be0d4b381cfe
Nickolas35 <86787140+Nickolas35@users.noreply.github.com>
2022-06-05 19:54:23 +0300

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.over(false)
+    music.playMelody("C5 B A G F E D C ", 120)
+    effects.clouds.startScreenEffect()
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level3`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonTeal, function (sprite, location) {
+    game.over(true)
+    music.powerUp.playUntilDone()
+})
 tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-scene.cameraFollowSprite(mySprite)
-controller.moveSprite(mySprite)
 let my_Enemy = sprites.create(assets.image`myImage0`, SpriteKind.Enemy)
 my_Enemy.follow(mySprite)
+forever(function () {
+    scene.cameraFollowSprite(mySprite)
+    controller.moveSprite(mySprite)
+})
